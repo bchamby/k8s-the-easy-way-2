002fab2aeb62ec0f318f199f07a410b738d6b5be
Brad Chamberlain <chamby@gmail.com>
2017-09-07 14:49:36 -0400

    adding cleanenv.sh script

---
+++ ansible/cleanenv.sh
@@ -0,0 +1,8 @@
+rm roles/workstation/files/*.pem
+rm roles/workstation/files/*.csr
+rm roles/controller/files/*.pem
+rm roles/controller/files/*.csr
+rm roles/worker/files/*.pem
+rm roles/worker/files/*.csr
+rm roles/worker/files/*.kubeconfig
+rm roles/workstation/templates/encryption-config.yaml
