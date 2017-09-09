5afa33bcc96f2633752030cc83bef504b1bab082
Brad Chamberlain <chamby@gmail.com>
2017-09-09 14:00:58 -0400

    made kubernetes worker version a variable

--- ansible/cleanenv.sh
+++ ansible/cleanenv.sh
@@ -1,8 +1,6 @@
+#!/bin/bash
 rm roles/workstation/files/*.pem
 rm roles/workstation/files/*.csr
-rm roles/controller/files/*.pem
-rm roles/controller/files/*.csr
-rm roles/worker/files/*.pem
-rm roles/worker/files/*.csr
-rm roles/worker/files/*.kubeconfig
+rm roles/controller/files/*.*
+rm roles/worker/files/*.*
 rm roles/workstation/templates/encryption-config.yaml
